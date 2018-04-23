529c8d2e5631f214957e82b6cfadd399386e6c33
WEIMAR <alexandertorreshe@gmail.com>
2018-04-23 09:19:01 -0400

    Login con datos

--- app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/LoginActivity.java
+++ app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/LoginActivity.java
@@ -1,18 +1,30 @@
 package com.programacion3.androidlatte.cafeteria_androidlatte;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private String usuario1 = "UPB";
+    private String usuario2 = "Case";
+    private String contraseña1 = "123";
+    private EditText usuario;
+    private EditText contraseña;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usuario = findViewById(R.id.usuario);
+        contraseña = findViewById(R.id.contraseña);
+
     }
 
     public void click(View view) {
@@ -20,7 +32,9 @@ public class LoginActivity extends AppCompatActivity {
         if (view.getId() == R.id.nuevo) {
             intent = new Intent(this, RegisterActivity.class);
         } else if (view.getId() == R.id.entrar) {
-            if (verificarDatos()) {
+            if (!esAdministrativo() && verificarDatos(usuario, contraseña)) {
+                intent = new Intent(this, MenuActivity.class);
+            } else if (esAdministrativo() && verificarDatos(usuario, contraseña)) {
                 intent = new Intent(this, MenuActivity.class);
             } else {
                 Toast.makeText(this, "Datos Erroneos", Toast.LENGTH_SHORT);
@@ -32,8 +46,21 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public boolean verificarDatos () {
-        return true;
+    public boolean verificarDatos(EditText usuario, EditText contraseña) {
+        if ((usuario1.equals(String.valueOf(usuario.getText())) || usuario2.equals(String.valueOf(usuario.getText()))) &&
+                contraseña1 == String.valueOf(contraseña.getText())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean esAdministrativo() {
+        if (usuario2.equals(String.valueOf(usuario.getText()))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }

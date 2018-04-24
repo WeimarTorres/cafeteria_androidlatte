ed981bef6af1f61681c2592d8c8f4038e9686962
Tincho <martin@loslalaguna.com>
2018-04-24 13:13:35 -0400

    Lista de Productos no terminada

--- app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/DisplayActivity.java
+++ app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/DisplayActivity.java
@@ -2,12 +2,22 @@ package com.programacion3.androidlatte.cafeteria_androidlatte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    List<Item> itemList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        listView = findViewById(R.id.lista1);
     }
 }

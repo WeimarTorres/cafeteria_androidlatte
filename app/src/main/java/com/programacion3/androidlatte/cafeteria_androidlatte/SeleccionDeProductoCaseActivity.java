689cfa1014a16fd2a82658c5210734ce6c7a2f24
sarau <sarisup14@gmail.com>
2018-04-24 13:20:15 -0400

    l

--- app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/SeleccionDeProductoCaseActivity.java
+++ app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/SeleccionDeProductoCaseActivity.java
@@ -2,12 +2,17 @@ package com.programacion3.androidlatte.cafeteria_androidlatte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SeleccionDeProductoCaseActivity extends AppCompatActivity {
+
+    ImageView imagen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_de_producto_case);
+
+        imagen = findViewById(R.id.comida);
     }
 }

175a39b5b27cd4e70200e95041a45aaa7c41c4a6
sarau <sarisup14@gmail.com>
2018-05-01 12:21:33 -0400

    activities

---
+++ app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/EdicionDeProductosActivity.java
@@ -0,0 +1,13 @@
+package com.programacion3.androidlatte.cafeteria_androidlatte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdicionDeProductosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edicion_de_productos);
+    }
+}
---
+++ app/src/main/java/com/programacion3/androidlatte/cafeteria_androidlatte/EditarMenuDelDiaActivity.java
@@ -0,0 +1,13 @@
+package com.programacion3.androidlatte.cafeteria_androidlatte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarMenuDelDiaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_menu_del_dia);
+    }
+}
